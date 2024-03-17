4ef3df6d4dfc5cfcb60df9e367348996ad7011de
Nsikan Simon <nsikansimon@gmail.com>
2024-03-17 23:28:56 +0100

    checking changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,19 +7,18 @@ import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title:
-    "Nsikan Simon Web Development Portfolio | Building Your Digital Experience.",
-  description:
-    "I develop digital experiences for brands across the globe, explore the range of projects I have successfully accomplished for my partners.",
-};
+// export const metadata = {
+//   title:
+//     "Nsikan Simon Web Development Portfolio | Building Your Digital Experience.",
+//   description:
+//     "I develop digital experiences for brands across the globe, explore the range of projects I have successfully accomplished for my partners.",
+// };
 
 export default function RootLayout({ children }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
 
-  
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
